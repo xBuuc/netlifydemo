49124f6515b462d10ef390ca1c26b714ce2573f4
xBuuc <112945277+xBuuc@users.noreply.github.com>
2023-03-09 12:26:42 +0100

    x

--- event.js
+++ event.js
@@ -1,5 +1,5 @@
-document.getElementById("click").addEventListener('click', () => {
-    setTimeout(() => {document.getElementById('yourImageId').style.display = 'block';},1000)
+document.getElementById("click").addEventListener('click', () => { document.getElementById('yourImageId').style.display = 'block';
+    setTimeout(() => {document.getElementById('yourImageId').style.display = 'none';},1000)
 }
 );
 
